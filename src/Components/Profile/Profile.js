d2373889335d75d9631e93683c272105ea1915bf
Mykhailo Kot <47431732+mykhailok007@users.noreply.github.com>
2020-01-31 23:40:15 +0200

    Add dynamic to Profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,7 +1,5 @@
 import React from "react";
-import classes from "./Profile.module.css";
 import Info from "./Info/Info";
-import MyPosts from "./MyPosts/MyPosts";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 function Profile(props) {
