4d9f802127a2c0776422eda1b818d1a550e0e811
Mykhailo Kot <47431732+mykhailok007@users.noreply.github.com>
2020-02-27 16:24:12 +0200

    Add componentDidUpdate

--- src/Components/ProfileStatus.js
+++ src/Components/ProfileStatus.js
@@ -18,6 +18,13 @@ class ProfileStatus extends React.Component {
                 status: e.currentTarget.value
             })
     }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.state !== this.props.status){
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
 
     render() {
         return <div>
